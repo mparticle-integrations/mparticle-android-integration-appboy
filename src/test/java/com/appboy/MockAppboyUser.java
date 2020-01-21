db02fdf6be32d61300356d27ac28f43cae7a46cf
Will Passidomo <wpassidomo@mparticle.com>
2020-01-21 10:10:26 -0500

    Apply Event Attribute to User Attribute mapping from KitConfig

--- src/test/java/com/appboy/MockAppboyUser.java
+++ src/test/java/com/appboy/MockAppboyUser.java
@@ -2,6 +2,11 @@ package com.appboy;
 
 import com.appboy.enums.Month;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import bo.app.bs;
 import bo.app.bv;
 import bo.app.dz;
@@ -21,5 +26,43 @@ public class MockAppboyUser extends AppboyUser {
         dobMonth = month;
         dobDay = day;
         return true;
+    }
+
+    private Map<String, List<String>> customAttributeArray = new HashMap<>();
+    private Map<String, String> customAttributes = new HashMap<>();
+
+    public Map<String, List<String>> getCustomAttributeArray() {
+        return customAttributeArray;
+    }
+
+    @Override
+    public boolean addToCustomAttributeArray(String key, String value) {
+        List<String> customArray = customAttributeArray.get(key);
+        if (customArray == null) {
+            customArray = new ArrayList<>();
+        }
+        customArray.add(value);
+        customAttributeArray.put(key, customArray);
+        return true;
+    }
+
+    @Override
+    public boolean removeFromCustomAttributeArray(String key, String value) {
+        try {
+            customAttributeArray.get(key).remove(value);
+            return true;
+        } catch (NullPointerException npe) {
+            return false;
+        }
+    }
+
+    @Override
+    public boolean setCustomUserAttribute(String key, String value) {
+        customAttributes.put(key, value);
+        return true;
+    }
+
+    public Map<String, String> getCustomUserAttributes() {
+        return customAttributes;
     }
 }
