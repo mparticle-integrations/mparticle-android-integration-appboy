48bb18419680e347a0e591dab026aaaf268db38d
wpassidomo <wpassidomo@mparticle.com>
2020-07-30 11:45:00 -0400

    Update Braze SDK to 8.1.0

--- src/test/java/com/appboy/MockAppboyUser.java
+++ src/test/java/com/appboy/MockAppboyUser.java
@@ -7,11 +7,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import bo.app.bs;
-import bo.app.bv;
-import bo.app.dz;
-import bo.app.ec;
-
 public class MockAppboyUser extends AppboyUser {
     public int dobYear = -1;
     public Month dobMonth = null;
